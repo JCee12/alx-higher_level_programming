7a835dffd6a0255d8ac03c0d33f184e094ebafb3
Jackline Muraya <jacklinewanja.jw@gmail.com>
2023-03-30 22:43:10 +0300

    Update 0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# displays the size of the body of the response
-curl -so /dev/null -w '%{size_download}\n' "$1"
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | awk '/Content-Length/{print $2}'
